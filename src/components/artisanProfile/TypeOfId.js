f64c36bec85b545bb69002086091770b634e4a40
Desarik <daniel.mejia@masiv.com>
2021-11-04 00:54:42 -0500

    refactoring errors

---
+++ src/components/artisanProfile/TypeOfId.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import {Dropdown} from "react-bootstrap";
+
+export const TypeOfId = (props) => {
+
+    const [type, setType] = useState([
+        {
+            name: 'CC',
+            description: 'CC',
+        },
+        {
+            name: 'TI',
+            description: 'TI',
+        },
+        {
+            name: 'CE',
+            description: 'CE',
+        },
+        {
+            name: 'PA',
+            description: 'PA',
+        },
+    ])
+
+    const [dropdownSelect, setDropdownSelect] = useState({
+            name: 'Seleccione una Categoria',
+            description: 'Seleccione una Categoria'
+        }
+    )
+
+    const handleClick = (item) => {
+        setDropdownSelect(item)
+    }
+
+    return (
+        <div>
+            <Dropdown className="m-3">
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    {dropdownSelect.name}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {
+                        type.map(item => (
+                            <Dropdown.Item onClick={() => handleClick(item)}>{item.name}</Dropdown.Item>
+                        ))
+                    }
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    )
+}
